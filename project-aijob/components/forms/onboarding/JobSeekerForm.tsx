a8266cade9e16bb0823f97c1d430584f8aa97e9d
LuckyLukas1 <lukas.blom02@gmail.com>
2025-05-04 10:26:13 +0200

    job seeker edit

--- project-aijob/components/forms/onboarding/JobSeekerForm.tsx
+++ project-aijob/components/forms/onboarding/JobSeekerForm.tsx
@@ -1,4 +1,6 @@
 import { jobSeekerSchema } from "@/app/utils/zodSchemas";
+import { UploadDropzone } from "@/components/general/UploadThingReexported";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -10,6 +12,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { XIcon } from "lucide-react";
+import Image from "next/image";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -24,7 +28,7 @@ export function JobSeekerForm() {
   });
   return (
     <Form {...form}>
-      <form>
+      <form className="space-y-4">
         <FormField
           control={form.control}
           name="name"
@@ -46,6 +50,51 @@ export function JobSeekerForm() {
               <FormLabel>Short bio</FormLabel>
               <FormControl>
                 <Textarea placeholder="Tell us about yourself..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="resume"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Resume (PDF)</FormLabel>
+              <FormControl>
+                <div>
+                  {field.value ? (
+                    <div className="relative w-fit">
+                      <Image
+                        src={field.value}
+                        alt="Company Logo"
+                        width={100}
+                        height={100}
+                        className="rounded-lg"
+                      />
+                      <Button
+                        type="button"
+                        variant="destructive"
+                        size="icon"
+                        className="absolute -top-2 -right-2"
+                        onClick={() => field.onChange("")}
+                      >
+                        <XIcon className="size-4" />
+                      </Button>
+                    </div>
+                  ) : (
+                    <UploadDropzone
+                      endpoint="imageUploader"
+                      onClientUploadComplete={(res) => {
+                        field.onChange(res[0].ufsUrl);
+                      }}
+                      onUploadError={(error) => {
+                        console.log("Something went wrong" + error);
+                      }}
+                      className="ut-button:bg-primary ut-button:text-white ut-button:hover:bg-primary/90 ut-label:text-muted-foreground ut-allowed-content:text-muted-foreground border-primary"
+                    />
+                  )}
+                </div>
               </FormControl>
               <FormMessage />
             </FormItem>
