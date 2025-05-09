aac649860e7ef30f29079c5704c49aa19a077f0e
LuckyLukas1 <lukas.blom02@gmail.com>
2025-05-09 12:14:49 +0200

    added slider

--- project-aijob/components/forms/CreateJobForm.tsx
+++ project-aijob/components/forms/CreateJobForm.tsx
@@ -1,5 +1,155 @@
-import { Form } from "../ui/form";
+"use client";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { jobSchema } from "@/app/utils/zodSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Card, CardContent, CardHeader } from "../ui/card";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { countryList } from "@/app/utils/countriesList";
+import { SalaryRangeSelector } from "../general/SalaryRangeSelector";
 
 export function CreateJobForm() {
-  return <Form></Form>;
+  const form = useForm<z.infer<typeof jobSchema>>({
+    resolver: zodResolver(jobSchema),
+    defaultValues: {
+      benefits: [],
+      companyAbout: "",
+      companyLocation: "",
+      companyName: "",
+      companyLogo: "",
+      companyWebsite: "",
+      companyXAccount: "",
+      employmentType: "",
+      jobDescription: "",
+      jobTitle: "",
+      listingDuration: 30,
+      location: "",
+      salaryFrom: 0,
+      salaryTo: 0,
+    },
+  });
+  return (
+    <Form {...form}>
+      <form className="col-span-1 lg:col-span-2 flex flex-col gap-8">
+        <Card>
+          <CardHeader>Job Information</CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="jobTitle"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Job Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="job title" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="employmentType"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Employment Type</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select Employment Type" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Employment Type</SelectLabel>
+                          <SelectItem value="full-time">Full Time</SelectItem>
+                          <SelectItem value="part-time">Part Time</SelectItem>
+                          <SelectItem value="contract">Contract</SelectItem>
+                          <SelectItem value="internship">Internship</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="location"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Job location</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select Location" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Worldwide</SelectLabel>
+                          <SelectItem value="worldwide">
+                            <span>🌍</span>
+                            <span className="pl-2">Worldwide / Remote</span>
+                          </SelectItem>
+                        </SelectGroup>
+                        <SelectGroup>
+                          <SelectLabel>Location</SelectLabel>
+                          {countryList.map((country) => (
+                            <SelectItem value={country.name} key={country.code}>
+                              <span>{country.flagEmoji}</span>
+                              <span className="pl-2">{country.name}</span>
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormItem>
+                <FormLabel>Salary Range</FormLabel>
+                <FormControl>
+                  <SalaryRangeSelector />
+                </FormControl>
+                <FormMessage>
+                  {form.formState.errors.salaryFrom?.message ||
+                    form.formState.errors.salaryTo?.message}
+                </FormMessage>
+              </FormItem>
+            </div>
+          </CardContent>
+        </Card>
+      </form>
+    </Form>
+  );
 }
---
+++ project-aijob/components/general/SalaryRangeSelector.tsx
@@ -0,0 +1,27 @@
+import { Control, useController } from "react-hook-form";
+import { Slider } from "../ui/slider";
+
+interface iAppProps {
+  control: Control;
+  minSalary: number;
+  maxSalary: number;
+  step: number;
+  currency: string;
+}
+
+export function SalaryRangeSelector({
+  control,
+  minSalary,
+  maxSalary,
+  step,
+  currency,
+}: iAppProps) {
+  const {} = useController({
+    name: "",
+  });
+  return (
+    <div className="w-full space-y-4">
+      <Slider max={100} step={1} />
+    </div>
+  );
+}
