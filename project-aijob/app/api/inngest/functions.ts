abd121e50a9beabfbeee5119629c3da619342fcd
LuckyLukas1 <lukas.blom02@gmail.com>
2025-06-05 20:53:57 +0200

    la till email utskick har kod kvar tills det är färdigt

--- project-aijob/app/api/inngest/functions.ts
+++ project-aijob/app/api/inngest/functions.ts
@@ -1,5 +1,8 @@
 import { prisma } from "@/app/utils/db";
 import { inngest } from "@/app/utils/inngest/client";
+import {Resend} from "resend"
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -40,11 +43,54 @@ export const sendPeriodicJobListings = inngest.createFunction(
         const {userId, email} = event.data;
 
         const totalDays = 30;
-        const intervalDays = 7;
+        const intervalDays = 6;
         let currentDay = 0;
 
         while(currentDay < totalDays){
             await step.sleep("wait-interval", `${intervalDays}d`)
+            currentDay += intervalDays;
+
+            const recentJobs = await step.run('fetch-recent-jobs', async () => {
+                return await prisma.jobPost.findMany({
+                    where: {
+                        status: "ACTIVE",
+                    },
+                    orderBy: {
+                        createdAt: "desc",
+                    },
+                    take: 10,
+                    include: {
+                        Company: {
+                            select:{
+                                name: true,
+                            },
+                        },
+                    },
+                });
+            });
+
+
+            if(recentJobs.length > 0) {
+                await step.run("send-email", async() => {
+                    const jobListingsHtml = recentJobs.map(
+                        (job) => 
+                        `<div style="margin-bottom: 20px; padding: 15px; border: 1px solid #eee; border-radius: 5px;">
+                            <h3 style="margin: 0;">${job.jobTitle}</h3>
+                            <p style="margin: 5px 0;">${job.Company.name} * ${job.location}</p>
+                            <p style="margin: 5px 0;">${job.salaryFrom.toLocaleString()} kr - ${job.salaryTo.toLocaleString()} kr</p>
+                        </div>                       
+                    `
+                    )
+                    .join("");
+
+                    await resend.emails.send({
+                        from: 'LuckyWork <onboarding@resend.dev>',
+                        to: ['lukas.blom02@gmail.com'],
+                        subject: "Nya jobb för dig",
+                        html: ``
+                    });
+                });
+            }
         }
     }
 )
