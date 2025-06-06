af1acf2ac3117b4e72edfdb562251daf6872f57a
LuckyLukas1 <lukas.blom02@gmail.com>
2025-06-06 20:03:34 +0200

    final klar del 1 av LuckyWork

--- project-aijob/app/(mainLayout)/my-jobs/page.tsx
+++ project-aijob/app/(mainLayout)/my-jobs/page.tsx
@@ -1,4 +1,38 @@
 import { prisma } from "@/app/utils/db";
+import { requireUser } from "@/app/utils/requireUser";
+import { EmptyState } from "@/components/general/EmptyState";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  CopyCheckIcon,
+  MoreHorizontal,
+  PenBoxIcon,
+  XCircle,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 async function getJobs(userId: string) {
   const data = await prisma.jobPost.findMany({
@@ -23,8 +57,103 @@ async function getJobs(userId: string) {
       createdAt: "desc",
     },
   });
+  return data;
 }
 export default async function MyJobsPage() {
-  const data = await getJobs();
-  return <h1></h1>;
+  const session = await requireUser();
+  const data = await getJobs(session.id as string);
+  return (
+    <>
+      {data.length === 0 ? (
+        <EmptyState
+          title="Inga jobbannonser hittades"
+          description="Du har inte skapat någon jobb annons"
+          buttonText="Create a job post now!"
+          href="/post-job"
+        />
+      ) : (
+        <Card>
+          <CardHeader>
+            <CardTitle>Mina jobb</CardTitle>
+            <CardDescription>
+              Hantera dina jobb listor och jobbansökningar här.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Logo</TableHead>
+                  <TableHead>Company</TableHead>
+                  <TableHead>Job Title</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Created at</TableHead>
+                  <TableHead>Actions</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {data.map((listing) => (
+                  <TableRow key={listing.id}>
+                    <TableCell>
+                      <Image
+                        src={listing.Company.logo}
+                        alt="logo of company"
+                        width={40}
+                        height={40}
+                        className="rounded-md size-10"
+                      />
+                    </TableCell>
+                    <TableCell>{listing.Company.name}</TableCell>
+                    <TableCell>{listing.jobTitle}</TableCell>
+                    <TableCell>
+                      {listing.status.charAt(0).toUpperCase() +
+                        listing.status.slice(1).toLowerCase()}
+                    </TableCell>
+                    <TableCell>
+                      {listing.createdAt.toLocaleDateString("en-US", {
+                        month: "long",
+                        day: "numeric",
+                        year: "numeric",
+                      })}
+                    </TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button variant="ghost" size="icon">
+                            <MoreHorizontal />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Åtgärder</DropdownMenuLabel>
+                          <DropdownMenuItem asChild>
+                            <Link href={`/my-jobs/${listing.id}/edit`}>
+                              <PenBoxIcon />
+                              Redigera jobb
+                            </Link>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem asChild>
+                            <Link href={`/my-jobs/${listing.id}/edit`}>
+                              <CopyCheckIcon />
+                              Kopiera jobb-URL
+                            </Link>
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem asChild>
+                            <Link href={`/my-jobs/${listing.id}/delete`}>
+                              <XCircle />
+                              Radera jobb
+                            </Link>
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      )}
+    </>
+  );
 }
